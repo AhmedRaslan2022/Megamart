c4ce57a40454c96f7c2a4391318c43d5bfbe1703
3zzam <mahmoudazzam954@gmail.com>
2022-07-01 11:08:59 +0200

    add setting ui

---
+++ Megamart/View/ViewControllers/SettingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingViewController.swift
+//  Megamart
+//
+//  Created by A on 10/04/1401 AP.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    @IBOutlet weak var switchTheme: UISwitch!
+    @IBOutlet weak var uiEditprofile: UIButton!
+    @IBOutlet weak var uiLogout: UIButton!
+    
+    @IBOutlet weak var uiLanguage: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
