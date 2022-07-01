600beacc9aa5857d16cd89c44c393b473b60d605
Your Name <you@example.com>
2022-07-01 04:26:00 +0200

    Home UI

---
+++ Megamart/View/ViewControllers/BrandsCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  BrandsCollectionCell.swift
+//  Megamart
+//
+//  Created by Macintosh on 01/07/2022.
+//
+
+import UIKit
+
+class BrandsCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Megamart/View/ViewControllers/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Megamart
+//
+//  Created by MAC on 29/06/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var AdsCollection: UICollectionView!
+    @IBOutlet weak var BrandsLabel: UILabel!
+    
+    @IBOutlet weak var BrandsCollection: UICollectionView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
