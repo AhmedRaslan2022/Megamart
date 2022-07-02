5cc6b6cb3dffc2cc1db69270d3d8a834e6b02e6d
Ahmed Raslan <you@example.com>
2022-07-02 13:00:24 +0200

    add Category Cell

---
+++ Megamart/View/ViewControllers/Cells/CategoryCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCollectionCell.swift
+//  Megamart
+//
+//  Created by Macintosh on 02/07/2022.
+//
+
+import UIKit
+
+class CategoryCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
