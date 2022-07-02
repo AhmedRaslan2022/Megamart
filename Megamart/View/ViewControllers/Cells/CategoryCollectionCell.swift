e2aa8fb396428eae7c6971bf9fd776eab85d8698
Ahmed Raslan <you@example.com>
2022-07-02 13:51:23 +0200

    finish Product,Category,Brands Cells

--- Megamart/View/ViewControllers/Cells/CategoryCollectionCell.swift
+++ Megamart/View/ViewControllers/Cells/CategoryCollectionCell.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class CategoryCollectionCell: UICollectionViewCell {
-
+    
+    @IBAction func FavouriteButton(_ sender: UIButton) {
+    }
+    @IBOutlet weak var ProductPrice: UILabel!
+    @IBOutlet weak var ProductImag: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
---
+++ Megamart/View/ViewControllers/Cells/ProductViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductViewCell.swift
+//  Megamart
+//
+//  Created by Macintosh on 02/07/2022.
+//
+
+import UIKit
+
+class ProductViewCell: UICollectionViewCell {
+    
+    @IBAction func FavouriteProduct(_ sender: UIButton) {
+    }
+    @IBOutlet weak var ProductPrice: UILabel!
+    @IBOutlet weak var ProductImage: UIImageView!
+}
