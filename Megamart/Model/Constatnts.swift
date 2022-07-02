f8756029cd12284f35588a49d90cdbd3e2c9abe5
Your Name <you@example.com>
2022-07-02 12:03:42 +0200

    add more constants and arrange file

--- Megamart/Model/Constatnts.swift
+++ Megamart/Model/Constatnts.swift
@@ -11,7 +11,8 @@ struct Constants {
 
     
 //MARK: -                   Storyboards
-        
+    static let Main_storyboard  = "Main"
+    static let Products_storyboard  = "Products"
     static let authentication_storyboard  = "Authentication"
     static let setting_storyboard  = "Setting"
     static let productDetails_storyboard  = "ProductDetails"
@@ -19,17 +20,19 @@ struct Constants {
         
     
 //MARK: -                   viewControllers
-    
+    static let HomeViewController_id = "Home_viewController_id"
+    static let CategoryViewController_id = "CategoryViewController_id"
     static let login_viewController_id = "login_viewController_id"
     static let SignUp_viewController_id = "SignUp_ViewController_id"
     static let Setting_viewController_id = "SettingViewController_id"
     static let ProductDetails_ViewController_id = "ProductDetails_ViewController_id"
     static let BagViewController_id = "BagViewController_id"
+    static let ProductsViewController_id = "Products_viewController_id"
     
 //MARK: -                   Cells
     
-    
-    static let ProductDetails_cell_id = "ProductDetails_cell_id"
+     static let Brands_Cell_id = "Brands_Cell_id"
+     static let ProductDetails_cell_id = "ProductDetails_cell_id"
      static let BagTableViewCell_id = "BagTableViewCell"
     
 
