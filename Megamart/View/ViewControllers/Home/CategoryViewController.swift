5e657ee8e6c01a68ad7f124d3b5368740351ca70
Your Name <you@example.com>
2022-07-02 11:44:31 +0200

    Add Product and Category UI

---
+++ Megamart/View/ViewControllers/Home/CategoryViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryViewController.swift
+//  Megamart
+//
+//  Created by Macintosh on 02/07/2022.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    @IBOutlet weak var CategoryCollection: UICollectionView!
+    
+    @IBAction func WomenButton(_ sender: UIButton) {
+    }
+    @IBAction func MenButton(_ sender: UIButton) {
+    }
+    @IBAction func KidsButton(_ sender: UIButton) {
+    }
+    @IBAction func SaleButton(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+
+
+}
---
+++ Megamart/View/ViewControllers/Product/ProductsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProductsViewController.swift
+//  Megamart
+//
+//  Created by Macintosh on 01/07/2022.
+//
+
+import UIKit
+
+class ProductsViewController: UIViewController {
+    @IBOutlet weak var SearchBar: UISearchBar!
+    @IBOutlet weak var PriceSlider: UISlider!
+    @IBOutlet weak var PriceLabel: UILabel!
+    
+    @IBOutlet weak var ProductCollection: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
