9f251f3161b32c2e79aa52fd0aa77cb51f63dc40
MohamedSaadKhalf <Mohamed.Saad.Khalfallah@gmail.com>
2022-07-03 20:22:07 +0200

    update in product details viewController

--- Megamart/View/ViewControllers/Cells/ProductDetails_CollectionViewCell.swift
+++ Megamart/View/ViewControllers/Cells/ProductDetails_CollectionViewCell.swift
@@ -13,7 +13,6 @@ import AlamofireImage
 class ProductDetails_CollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak private var product_imageView: UIImageView!
-    @IBOutlet weak private var addToFavorites_button: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,18 +23,9 @@ class ProductDetails_CollectionViewCell: UICollectionViewCell {
         Alamofire.request(imageUrl).responseImage { response in
             if case .success(let image) = response.result {
                 self.product_imageView.image = image
-            }else{
-                print("###############################################")
             }
         }
+        
     }
-//    AF.request(url).responseImage { response in
-//        if case .success(let image) = response.result {
-//            self.recipeImage.image = image
-//        }
-//    }
 
-    @IBAction func addToFavorites_button(_ sender: UIButton) {
-        addToFavorites_button.imageView?.image = UIImage(systemName: "heart.fill")
-    }
 }
