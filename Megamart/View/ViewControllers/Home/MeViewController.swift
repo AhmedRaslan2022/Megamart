aefefc42d4d1b970e4c52e05d81aeb00cabc47e5
MohamedSaadKhalf <Mohamed.Saad.Khalfallah@gmail.com>
2022-07-13 18:23:55 +0200

    add logout to settings viewController

--- Megamart/View/ViewControllers/Home/MeViewController.swift
+++ Megamart/View/ViewControllers/Home/MeViewController.swift
@@ -16,6 +16,11 @@ class MeViewController: UIViewController {
     }
     
 
-    
+    @IBAction func goSettings_ViewController(_ sender: UIBarButtonItem) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: Constants.setting_storyboard, bundle:nil)
+        let settingsVC = storyBoard.instantiateViewController(withIdentifier: Constants.Setting_viewController_id) as! SettingViewController
+        settingsVC.modalPresentationStyle = .fullScreen
+        self.present(settingsVC, animated: true, completion: nil)
+    }
 
 }
